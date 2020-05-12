490fb507d74e02f5f998f2ebc68f8d09df961cfe
Muhammad Farag <muhammad.farag@shopify.com>
2020-05-12 01:23:14 -0400

    Displays Login Screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import '@shopify/react-testing/matchers';
+import Login from "./components/Login";
+import {mount} from "@shopify/react-testing";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  it('displays login screen', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toContainReactComponent(Login);
+  });
+})
