f0f98864ee39cf9f4f51e8ba0bd6733dea37be66
Muhammad Farag <muhammadfarag@gmail.com>
2020-05-17 10:42:17 -0400

    refactor: Separate answered and unanswered questions returns

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,16 +2,29 @@ import * as PropTypes from "prop-types";
 import React from "react";
 
 export function Question({activeQuestion, author, activeUser}) {
-  const answerd1 = activeQuestion.optionOne.votes.includes(activeUser.id)
-  const answerd2 = activeQuestion.optionTwo.votes.includes(activeUser.id)
+  const activeUserAnsweredOptionOne = activeQuestion.optionOne.votes.includes(activeUser.id)
+  const activeUserAnsweredOptionTwo = activeQuestion.optionTwo.votes.includes(activeUser.id)
 
+
+
+  if(activeUserAnsweredOptionOne || activeUserAnsweredOptionTwo){
+    return <>
+      <div>
+        {author.name} Asked
+      </div>
+      <ul>
+        <li>{activeQuestion.optionOne.text} {activeUserAnsweredOptionOne ? "*" : null}</li>
+        <li>{activeQuestion.optionTwo.text} {activeUserAnsweredOptionTwo ? "*" : null}</li>
+      </ul>
+    </>;
+  }
   return <>
     <div>
       {author.name} Asked
     </div>
     <ul>
-      <li>{activeQuestion.optionOne.text} {answerd1 ? "*" : null}</li>
-      <li>{activeQuestion.optionTwo.text} {answerd2 ? "*" : null}</li>
+      <li>{activeQuestion.optionOne.text}</li>
+      <li>{activeQuestion.optionTwo.text}</li>
     </ul>
   </>;
 }
