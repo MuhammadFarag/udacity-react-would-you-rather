ab4388d23a26b3a6e2e8346780d2cea9d2ebe7d1
Muhammad Farag <muhammadfarag@gmail.com>
2020-05-17 11:38:57 -0400

    feat(Unanswered Question Page): Wire radio buttons
    When one radio button is selected the other is unselected

--- src/components/UnansweredQuestion.js
+++ src/components/UnansweredQuestion.js
@@ -1,7 +1,13 @@
 import * as PropTypes from "prop-types";
-import React from "react";
+import React, {useState} from "react";
 
 export function UnansweredQuestion({activeQuestion, author, activeUser}) {
+  const [selectedOption, setSelectedOption] = useState("")
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value)
+  }
+
   return <>
     <div>
       {author.name} Asked
@@ -11,13 +17,13 @@ export function UnansweredQuestion({activeQuestion, author, activeUser}) {
     }}>
       <div>
         <label>
-          <input type="radio" value="optionOne"/>
+          <input type="radio" value="optionOne" checked={selectedOption === "optionOne"} onChange={handleChange}/>
           {activeQuestion.optionOne.text}
         </label>
       </div>
       <div>
         <label>
-          <input type="radio" value="optionTwo"/>
+          <input type="radio" value="optionTwo" checked={selectedOption === "optionTwo"} onChange={handleChange}/>
           {activeQuestion.optionTwo.text}
         </label>
       </div>
