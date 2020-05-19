acceb2541410993836c0537b045446f74421c841
Muhammd Farag <muhammadfarag@gmail.com>
2020-05-19 13:48:47 -0400

    style(Login): Add avatars and style login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {authenticateUser} from "../redux-stuff";
+import Image from "react-bootstrap/Image";
+import {ListGroup} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
-function Login({history}) {
+function Login() {
   const dispatch = useDispatch()
   const users = useSelector(state => Object.values(state.users))
 
@@ -11,17 +16,22 @@ function Login({history}) {
     dispatch(authenticateUser(user))
   }
 
-  return <ul>
-    {users.map((user) => (
-      <li key={user.id}>
-        <button onClick={() => {
-          handleAuthentication(user)
-          history.push('/unanswered-questions')
-        }
-        }>{user.name}</button>
-      </li>
-    ))}
-  </ul>;
+  return <Container>
+    <Row className="justify-content-md-center">
+      <Col md="auto">
+        <ListGroup>
+          {users.map((user) => (
+            <ListGroup.Item key={user.id}>
+              <Image src={user.avatarURL} roundedCircle/>
+              <Link to='/unanswered-questions' onClick={() => {
+                handleAuthentication(user)
+              }}>{user.name}</Link>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Col>
+    </Row>
+  </Container>;
 }
 
 export default withRouter(Login);
