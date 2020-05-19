048a34ce2acaca3e191787da02fcf98585812475
Muhammd Farag <muhammadfarag@gmail.com>
2020-05-19 11:14:36 -0400

    style: Navigation links layout

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {Nav, Navbar} from "react-bootstrap";
+import Logout from "./Logout";
 
 export function Navigation() {
-  return <div>
-    <ul>
-      <li>
-        <Link to='/unanswered-questions'>
-          Unanswered Questions
-        </Link>
-      </li>
-      <li>
-        <Link to='/answered-questions'>
-          Answered Questions
-        </Link>
-      </li>
-      <li>
-        <Link to='/add'>
-          New Question
-        </Link>
-      </li>
-      <li>
-        <Link to='/leaderboard'>
-          Leader Board
-        </Link>
-      </li>
-    </ul>
-  </div>;
+  return <Navbar>
+    <Nav className="mr-auto">
+      <Nav.Item>
+        <Link class="nav-link" to='/unanswered-questions'>Unanswered Questions</Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link class="nav-link" to='/answered-questions'>Answered Questions</Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link class="nav-link" to='/add'>New Question</Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link class="nav-link" to='/leaderboard'>Leader Board</Link>
+      </Nav.Item>
+    </Nav>
+    <Nav>
+      <Logout/>
+    </Nav>
+  </Navbar>;
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,19 +1,11 @@
 import {Navigation} from "./Navigation";
-import Logout from "./Logout";
 import React from "react";
 import {Col, Container} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 
 export function Page({children}) {
   return <Container>
-    <Row>
-      <Col>
-        <Navigation/>
-      </Col>
-      <Col md="auto">
-        <Logout/>
-      </Col>
-    </Row>
+    <Navigation/>
     <Row>
       <Col>
         {children}
