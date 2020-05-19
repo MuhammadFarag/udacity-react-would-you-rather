f8e35992cf6555462d0d87804f5c539f1abf9c79
Muhammd Farag <muhammadfarag@gmail.com>
2020-05-19 14:10:34 -0400

    style: question listitem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,28 @@
 import * as PropTypes from "prop-types";
 import React from "react";
 import {Link, withRouter} from "react-router-dom";
+import Media from "react-bootstrap/Media";
 
 function ListItem({author, onClick, question}) {
-  return <li>
-    {author.name} Asked:
-    <ul>
-      <li>{question.optionOne.text}</li>
-      <li>{question.optionTwo.text}</li>
-    </ul>
-    <Link to={`/questions/${question.id}`} onClick={onClick}>View</Link>
-  </li>;
+  return (<div>
+    <Media>
+      <img
+        width={64}
+        height={64}
+        className="mr-3"
+        src={author.avatarURL}
+        alt={author.name}
+      />
+      <Media.Body>
+        <h5>{author.name} Asked</h5>
+        <div>Would you rather</div>
+        <div className="font-weight-bold">{question.optionOne.text}?</div>
+        <div>or</div>
+        <div className="font-weight-bold">{question.optionTwo.text}?</div>
+        <Link to={`/questions/${question.id}`} onClick={onClick}>View</Link>
+      </Media.Body>
+    </Media>
+  </div>);
 }
 
 ListItem.propTypes = {
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -6,12 +6,10 @@ export function QuestionList({questions}) {
   const users = useSelector(state => state.users)
 
   return <div>
-    <ol>
       {questions.sort((a, b) => b.timestamp - a.timestamp).map((question) => (
         <ListItem key={question.id} author={users[question.author]} question={question} onClick={() => {
         }}/>
       ))}
-    </ol>
 
   </div>
 
