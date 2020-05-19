3af94210707fd0f1dc6315fe53d82324f7341291
Muhammd Farag <muhammadfarag@gmail.com>
2020-05-19 15:36:52 -0400

    style: Leader board

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,5 +1,8 @@
 import {useSelector} from "react-redux";
 import React from "react";
+import Media from "react-bootstrap/Media";
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
 
 export function LeaderBoard() {
   const users = useSelector(state => Object.values(state.users).map(user => ({
@@ -7,22 +10,33 @@ export function LeaderBoard() {
     name: user.name,
     answeredQuestions: Object.keys(user.answers).length,
     createdQuestions: user.questions.length,
-    score: Object.keys(user.answers).length + user.questions.length
+    score: Object.keys(user.answers).length + user.questions.length,
+    avatarURL: user.avatarURL
   }))).sort((a, b) => b.score - a.score)
 
   return <div>
-    <ul>
+    <ListGroup>
       {users.map(user => {
-        return <li key={user.id}>
-          <ul>
-            <li>Name: {user.name}</li>
-            <li>Answered questions: {user.answeredQuestions}</li>
-            <li>Created questions: {user.createdQuestions}</li>
-            <li>Score: {user.score} </li>
-          </ul>
-        </li>
+        return (
+          <ListGroup.Item key={user.id}>
+            <Media>
+              <img
+                width={64}
+                height={64}
+                className="mr-3"
+                src={user.avatarURL}
+                alt={user.name}
+              />
+              <Media.Body>
+                <h5>{user.name}</h5>
+                <div>Answered questions: <Badge variant="secondary">{user.answeredQuestions}</Badge></div>
+                <div>Created questions: <Badge variant="secondary">{user.createdQuestions}</Badge></div>
+                <div>Score: <Badge variant="success">{user.score}</Badge></div>
+              </Media.Body>
+            </Media>
+          </ListGroup.Item>)
       })}
-    </ul>
+    </ListGroup>
   </div>
 
 }
