9e4bad026333cf8c40ad94042e147c423fec28ee
Muhammd Farag <muhammadfarag@gmail.com>
2020-05-19 10:46:40 -0400

    style: Page layout

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,11 +1,23 @@
 import {Navigation} from "./Navigation";
 import Logout from "./Logout";
 import React from "react";
+import {Col, Container} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
 
 export function Page({children}) {
-  return <div>
-    <Navigation/>
-    <Logout/>
-    {children}
-  </div>
+  return <Container>
+    <Row>
+      <Col>
+        <Navigation/>
+      </Col>
+      <Col md="auto">
+        <Logout/>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        {children}
+      </Col>
+    </Row>
+  </Container>
 }
