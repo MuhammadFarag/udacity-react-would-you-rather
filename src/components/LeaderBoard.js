6873af1f34679b4bf3fcbd760630d62d91b21bfe
Muhammd Farag <muhammadfarag@gmail.com>
2020-05-19 06:08:34 -0400

    feat: Display the leaderboard with highest score players first (descending order)

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -2,7 +2,13 @@ import {useSelector} from "react-redux";
 import React from "react";
 
 export function LeaderBoard() {
-  const users = useSelector(state => Object.values(state.users))
+  const users = useSelector(state => Object.values(state.users).map(user => ({
+    id: user.id,
+    name: user.name,
+    answeredQuestions: Object.keys(user.answers).length,
+    createdQuestions: user.questions.length,
+    score: Object.keys(user.answers).length + user.questions.length
+  }))).sort((a, b) => b.score - a.score)
 
   return <div>
     <ul>
@@ -10,9 +16,9 @@ export function LeaderBoard() {
         return <li key={user.id}>
           <ul>
             <li>Name: {user.name}</li>
-            <li>Answered questions: {Object.keys(user.answers).length}</li>
-            <li>Created questions: {user.questions.length}</li>
-            <li>Score: {Object.keys(user.answers).length + user.questions.length} </li>
+            <li>Answered questions: {user.answeredQuestions}</li>
+            <li>Created questions: {user.createdQuestions}</li>
+            <li>Score: {user.score} </li>
           </ul>
         </li>
       })}
